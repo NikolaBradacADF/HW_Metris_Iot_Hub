429b48be2f96181c771e121550d9a27109401fec
Nikola Bradac <nikola.bradac@gmail.com>
2023-01-27 13:40:48 +0100

    Changes to bat file for merging, correct var name in IO

---
+++ cloneCoreSW.bat
@@ -0,0 +1,18 @@
+@echo git clone Metris_Iot_Hub repo int _temp
+@echo dir %~dp0
+pause
+cd %~dp0
+@echo dir %CD%
+pause
+git clone git@github.com:NikolaBradacADF/Metris_Iot_Hub.git .temp\
+pause
+Xcopy %CD%\.temp\MetrisIotHub\MetrisIotHub\CONFIG %CD%\MetrisIotHub\MetrisIotHub\CONFIG  /E /H /I
+Xcopy %CD%\.temp\MetrisIotHub\MetrisIotHub\DUTs %CD%\MetrisIotHub\MetrisIotHub\DUTs  /E /H /I
+Xcopy %CD%\.temp\MetrisIotHub\MetrisIotHub\GVLs %CD%\MetrisIotHub\MetrisIotHub\GVLs  /E /H /I
+Xcopy %CD%\.temp\MetrisIotHub\MetrisIotHub\HMI %CD%\MetrisIotHub\MetrisIotHub\HMI  /E /H /I
+Xcopy %CD%\.temp\MetrisIotHub\MetrisIotHub\POUs %CD%\MetrisIotHub\MetrisIotHub\POUs  /E /H /I
+copy %CD%\.temp\MetrisIotHub\MetrisIotHub\MetrisIotHub.plcproj %CD%\MetrisIotHub\MetrisIotHub\MetrisIotHub.plcproj 
+
+
+
+pausese
